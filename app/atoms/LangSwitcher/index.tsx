e94b2f87213e05e7d5e5e8f462fc51ce23e4de14
Richard Miguel Irala <richardirala47@hotmail.com>
2026-03-29 20:09:45 -0300

    Improve language switcher

--- app/atoms/LangSwitcher/index.tsx
+++ app/atoms/LangSwitcher/index.tsx
@@ -1,17 +1,45 @@
+import { useCallback, useState } from "react";
 import { useLanguage } from "../../context/LanguageContext";
+import type { Lang } from "~/types";
 
 function LangSwitcher() {
   const { lang, setLang } = useLanguage();
+  const [showOptions, setShowOptions] = useState<boolean>(false);
+
+  const changeLanguage = useCallback((value: Lang) => {
+    setLang(value);
+    setShowOptions(false);
+  }, []);
 
   return (
-    <button
-      className="langswitcher__button--option"
-      onClick={() => setLang(lang === "en" ? "es" : "en")}
-    >
-      {lang === "en"
-        ? "Cambiar idioma a Español"
-        : "Switch Language to English"}
-    </button>
+    <div>
+      <button
+        className="langswitcher__button--option"
+        onClick={() => setShowOptions(!showOptions)}
+      >
+        {lang === "en" ? "Language: English" : "Idioma: Español"}
+      </button>
+
+      <div className="langswitcher__relativeContainer">
+        {showOptions && (
+          <div className="langswitcher__optionsContainer">
+            <div
+              className="langswitcher__option"
+              onClick={() => changeLanguage("en")}
+            >
+              English
+            </div>
+            <div className="langswitcher__horizontalLine"></div>
+            <div
+              className="langswitcher__option"
+              onClick={() => changeLanguage("es")}
+            >
+              Español
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
   );
 }
 
